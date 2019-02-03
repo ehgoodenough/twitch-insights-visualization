abef39cf9190c2431b31b11a7ca9a878d18ca43c
Andrew McPherson <ehgoodenough@gmail.com>
2019-02-03 14:17:18 -0800

    Added basic data grokking.

--- TODO.md
+++ TODO.md
@@ -1,11 +1,9 @@
 Firstly:
-- Add the drag-and-drop or upload method.
-- Parse the excel spreadsheet as json?
-- Render the name of the extension.
 - Show the funnel for the last 30 days.
 - Create an upload process for github pages.
 
 Later:
+- Add a file picker form instead of just drag-and-drop.
 - Add controls for funnel within 7 days or 1 day.
 - Add the ability to compare 2 insights, like benchmarks.
 - Consider how to show churn (deactivate/uninstall, minimize).
--- source/views/Mount.view.js
+++ source/views/Mount.view.js
@@ -5,30 +5,75 @@ import dragdrop from "drag-drop/buffer"
 
 import "views/Mount.view.less"
 
+let insights = []
+
 export default class Mount {
     render() {
         return (
             <div id="mount">
-                Hello World!!
+                {this.content}
+            </div>
+        )
+    }
+    get content() {
+        if(insights.length === 0) {
+            return (
+                <div>
+                    Drag-and-drop your Twitch Insights CSV.
+                </div>
+            )
+        }
+        if(insights[0].type === "Extension") {
+            return (
+                <ExtensionInsight insight={insights[0]}/>
+            )
+        }
+        return (
+            <div>
+                We don't yet support that CSV format!
             </div>
         )
     }
     componentDidMount() {
         dragdrop("#mount", (files) => {
             files.forEach((file) => {
+                // Create a new insight object.
+                let insight = {
+                    "filename": file.name,
+                    "filesize": file.size,
+                }
+
+                // Attempt to parse the insights data.
                 try {
-                    let contents = files[0].toString("utf8")
-                    contents = csvparse(contents, {
-                        "cast": true,
-                        "columns": true,
+                    insight.data = file.toString("utf8")
+                    insight.data = csvparse(insight.data, {
+                        "cast": true, "columns": true
                     })
-                    console.log(contents[0])
-                    console.log(contents[1])
-                    console.log(contents[2])
                 } catch(error) {
                     console.error(`Could not parse "${file.name}"`)
+                    console.error(error)
+                    return
                 }
+
+                // Recognize the insights type.
+                if(!!insight.data[0]["Extension Name"]) {
+                    insight.type = "Extension"
+                }
+
+                // Add the insights to the store.
+                insights.unshift(insight)
             })
         })
+    }
+}
+
+class ExtensionInsight {
+    render() {
+        return (
+            <div class="Insight">
+                <h1>{this.props.insight.data[0]["Extension Name"]}</h1>
+                <h3>{this.props.insight.data[0]["Extension Client ID"]}</h3>
+            </div>
+        )
     }
 }
